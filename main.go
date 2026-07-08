b164fb436671310e8bf9fd9f103daa451807a117
Conner Hammonds <connerhammonds@yahoo.com>
2026-07-08 19:40:09 +0000

    Initial func declaration in main

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import "os"
+
+func main() {
+	switch os.Args[1] {
+	case "run":
+		parent()
+	case "child":
+		child()
+	default:
+		panic("what should I do?")
+	}
+}
+
+func parent() {
+}
+
+func child() {
+}
+
+func must() {
+}
